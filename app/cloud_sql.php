31603c9b5a9453afe543bf820321187ce9df3aa5
Riccardo Carlesso <palladiusbonton+git@gmail.com>
2013-08-12 11:39:14 +0200

    funzionaaaaaaaaaa

--- app/cloud_sql.php
+++ app/cloud_sql.php
@@ -10,7 +10,7 @@ Output: <br/>
 # http://stackoverflow.com/questions/17025605/google-app-engine-php-and-cloud-sql
 
 #  CREATE USER 'riccardo'@'localhost' IDENTIFIED BY 'attenzione_pubblicissima';
-#  GRANT ALL PRIVILEGES ON * . * TO 'riccardo'@'localhost';
+#  GRANT ALL PRIVILEGES ON provapaccap.* TO 'riccardo'@'localhost';
 #  FLUSH PRIVILEGES;
 
 # Creato DB google.com:discoproject:provapaccap attaccato alla app 'provapaccap'
@@ -34,9 +34,27 @@ Output: <br/>
             MYSQL_LOGIN,
             MYSQL_PASSWORD
         );
+
+
     }catch(Exception $e){
+        print "Exception! ";
         var_dump($e->__toString());
     }
 
+
+// some code
+    $sql = "SELECT * FROM posts";
+    print "Query: $sql\n" ;
+    $q = mysql_query($sql,$db);
+    print "Query results: \n" ;
+    print_r($q);
+    
+    foreach($db->query($sql) as $row) {
+      echo "row:: $row \n";
+      print_r($row);
+      
+      echo $row['title'].': '.$row['body']; //etc...
+    }
+    mysql_close($con);
 ?>
 </pre>
--- app/posts.php
+++ app/posts.php
@@ -1,5 +1,31 @@
 
-<h1>Blog</h1>
+<h1>Posts</h1>
 
-TODO metti tutti gli oggetti di tipo Post
+Vedi tutti gli oggetti di tipo Post
 
+<?php
+    define('MYSQL_HOST', "/cloudsql/google.com:discoproject:provapaccap");
+    define('MYSQL_LOGIN', "riccardo");
+    define('MYSQL_PASSWORD', "attenzione_pubblicissima");
+    #define('MYSQL_DB', "riccardo_test"); # 
+    define('MYSQL_DB', "provapaccap");
+    try{
+        $db = new PDO('mysql:unix_socket='.MYSQL_HOST.';dbname='.MYSQL_DB.';charset=utf8', MYSQL_LOGIN, MYSQL_PASSWORD);
+    } catch(Exception $e) {
+        print "Exception! ";
+        var_dump($e->__toString());
+    }
+    $sql = "SELECT * FROM posts";
+    foreach($db->query($sql) as $row) {
+?>
+    <h2 class='posts_title' >
+        <? print $row['title'] ?>
+    </h2>
+    <em class='posts_body'>
+        <? print  $row['body'] ?>
+    </em>
+<?
+      # print_r($row);
+    }
+    mysql_close($con);
+?>
